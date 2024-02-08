1c1b22c6aba7e488eeb8767d4ea6125d6b525701
Toma1345 <thomas.brossier@etu.univ-orleans.fr>
2024-02-08 09:13:43 +0100

    Journee.java : scoreAuteur() & getNomrbeSpectacle

--- Journee.java
+++ Journee.java
@@ -2,17 +2,17 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class Journee{
-    private Map<String,Integer> nbSpec;
+    private Map<String,Integer> nbreSpectacle;
     private Map<String,Style> style;
 
 
     public Journee(Style matin, int specMatin, Style apresMidi, int specApresMidi, Style soiree, int specSoiree) {
-        this.nbSpec = new HashMap<>();
+        this.nbreSpectacle = new HashMap<>();
         this.style = new HashMap<>();
         
-        this.nbSpec.put("Matin",specMatin);
-        this.nbSpec.put("ApresMidi",specApresMidi);
-        this.nbSpec.put("Soiree",specSoiree);
+        this.nbreSpectacle.put("Matin",specMatin);
+        this.nbreSpectacle.put("ApresMidi",specApresMidi);
+        this.nbreSpectacle.put("Soiree",specSoiree);
 
         this.style.put("Matin",matin);
         this.style.put("ApresMidi",apresMidi);
@@ -22,14 +22,17 @@ public class Journee{
     public Style getStyle(Periode periode) {
         return this.style.get(periode.toString());
     }
+    public int getNombreSpectacle(Periode periode){
+        return this.nbreSpectacle.get(periode.toString());
+    }
 
 
     public int scoreJournee( Auteur auteur ){
         return 0;
     }
 
-    public int scoreAuteur(Auteur auteur, Journee epreuve){
-        return 0;
+    public int scoreAuteur(Auteur auteur, Periode epreuve){
+        return auteur.qualiteStyle(this.style.get(epreuve.toString()))*this.getNombreSpectacle(epreuve);
     }
 
 }
