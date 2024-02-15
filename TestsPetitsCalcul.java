5dce9d5b397a0418cf38ae68866a4ab70c17f31e
Toma1345 <thomas.brossier@etu.univ-orleans.fr>
2024-02-15 11:40:15 +0100

    Test corrigé : Multiplication2 et Hexa

--- TestsPetitsCalcul.java
+++ TestsPetitsCalcul.java
@@ -14,11 +14,11 @@ public class TestsPetitsCalcul {
 
     @Test
     public void testMultiplication2(){
-        assertEquals(3252, 32 * 52);
+        assertEquals(1664, 32 * 52);
     }
 
      @Test
      public void testHexa(){
-        assertEquals(1024, 0x10 * 0x10);
+        assertEquals(256, 0x10 * 0x10);
      }
 }
